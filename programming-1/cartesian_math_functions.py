d82d32c08e6a47b64f43c84e55fb9fd45befc995
russell-codingishard <rchow10@jhu.edu>
2021-10-25 13:16:12 -0400

    setup todo for library of functions

---
+++ programming-1/cartesian_math_functions.py
@@ -0,0 +1,25 @@
+# Create a library of essential functions for 3D point manipulation and Cartesian transformations
+
+# Define identity matrix
+
+# Define rotation matrix
+
+# Rotation angle and rotation from rotation matrix
+
+# Product of rotation matrix and vector
+
+# Product of frame and vector
+
+# Cross product of two vectors
+
+# Dot product of two vectors
+
+# Inverse of rotation matrix
+
+# Inverse of frame
+
+# Product of two rotation matrices
+
+# Product of two frames
+
+# Least squares solver
