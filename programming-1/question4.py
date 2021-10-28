55efcdca40b2d1449f83f9915b4a4d14fe2d66cd
russell-codingishard <rchow10@jhu.edu>
2021-10-27 23:52:37 -0400

    fix read inputs

--- programming-1/question4.py
+++ programming-1/question4.py
@@ -1,23 +1,26 @@
 # Question 4
 # Solve for distorted calibration
 
-# Get user input for file name:
 import numpy as np
+from cartesian import *
+from icp import *
+
+# Get user input for file name:
 print('Enter the name of your input data file (e.g. pa1-debug-a): ')
 filename = input()
 
 #Get data from the input files
-calBodyData,calBodySize = readInputFile(filename + '-calbody.txt')
-calReadData,calReadSize = readInputFile(filename + '-calreadings.txt')
+calBodyData,calBodySize = readInput_Body(filename + '-calbody.txt')
+calReadData,calReadSize = readInput_Readings(filename + '-calreadings.txt')
 N_D = calBodySize(1)
 N_A = calBodySize(2)
 N_C = calBodySize(3)
 N_framescal = calReadSize(4)
 
 # Position of markers with respect to the calibration body
-d = calBodyData[np.arange(1,N_D+1),:]
+d = calBodyData[np.arange(N_D+1,1),:]
 a = calBodyData[np.arange(N_D + 1,N_A + N_D+1),:]
-c = calBodyData[np.arange(N_D + N_A + 1,end()+1),:]
+c = calBodyData[np.arange(N_D + N_A + 1,len()-1),:]
 
 # Position of markers with respect to the trackers
 D = np.zeros((N_D,3,N_framescal))
@@ -40,7 +43,7 @@ for i in np.arange(1,N_framescal+1).reshape(-1):
 R_D = np.zeros((3,3,N_framescal))
 p_D = np.zeros((3,N_framescal))
 for i in np.arange(1,N_framescal+1).reshape(-1):
-    R_i,p_i = registration(d,D[:,:,i])
+    R_i,p_i = ICP(d,D[:,:,i])
     R_D[:,:,i] = R_i
     p_D[:,i] = p_i
 
@@ -51,7 +54,7 @@ for i in np.arange(1,N_framescal+1).reshape(-1):
 R_A = np.zeros((3,3,N_framescal))
 p_A = np.zeros((3,N_framescal))
 for i in np.arange(1,N_framescal+1).reshape(-1):
-    R_i,p_i = registration(a,A[:,:,i])
+    R_i,p_i = ICP(a,A[:,:,i])
     R_A[:,:,i] = R_i
     p_A[:,i] = p_i
 
@@ -59,7 +62,7 @@ for i in np.arange(1,N_framescal+1).reshape(-1):
 C_exp = np.zeros((N_C,3,N_framescal))
 for i in np.arange(1,N_framescal+1).reshape(-1):
     for j in np.arange(1,N_C+1).reshape(-1):
-        C_exp[j,:,i] = np.transpose((inv(R_D[:,:,i]) * (R_A[:,:,i] * np.transpose(c(j,:)) + p_A[:,i] - p_D[:,i])))
+        C_exp[j,:,i] = np.transpose((np.linalg.inv(R_D[:,:,i]) * (R_A[:,:,i] * np.transpose(c(j,:)) + p_A[:,i] - p_D[:,i])))
 
 ## Part D: Prepare for output file
 # Reshaping C_exp
