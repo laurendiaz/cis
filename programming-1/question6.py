f64c605f9a41fb75f40049e94023ad0636839bd3
russell-codingishard <rchow10@jhu.edu>
2021-10-29 02:42:39 -0400

    fix for loops

--- programming-1/question6.py
+++ programming-1/question6.py
@@ -22,7 +22,7 @@ D = np.zeros((N_D, 3, N_frames))
 H = np.zeros((N_H, 3, N_frames))
 
 ind = 0
-for i in np.arange(1, N_frames).reshape(-1):
+for i in np.arange(0, N_frames):
     D[:, :, i] = optPivotData[np.arange(ind, ind + N_D), :]
     ind = ind + N_D
     H[:, :, i] = optPivotData[np.arange(ind, ind + N_H), :]
@@ -33,7 +33,7 @@ for i in np.arange(1, N_frames).reshape(-1):
 # p_D is a 3XN_frames 2D matrix, each column corresponds to the translation of a frame
 R_D = np.zeros((3, 3, N_frames))
 p_D = np.zeros((3, N_frames))
-for i in np.arange(1, N_frames + 1).reshape(-1):
+for i in np.arange(0, N_frames):
     R_i, p_i = ICP(d, D[:, :, i], F0, eta0)
     R_D[:, :, i] = R_i
     p_D[:, i] = p_i
@@ -41,8 +41,8 @@ for i in np.arange(1, N_frames + 1).reshape(-1):
 # Transform the optical tracker beacon positions into EM tracker coordinates
 P = np.zeros((N_H, 3, N_frames))
 
-for i in np.arange(1, N_frames + 1).reshape(-1):
-    for j in np.arange(1, N_H + 1).reshape(-1):
+for i in np.arange(0, N_frames):
+    for j in np.arange(0, N_H):
         P[j, :, i] = np.transpose((np.linalg.solve(R_D[:, :, i], (np.transpose(H[j, :, i]))) - np.linalg.solve(R_D[:, :, i], p_D[:, i])))
 
 # Define local probe coordinate system and compute p_i using the first frame
@@ -51,7 +51,7 @@ P_0 = np.mean(P1, 1)
 
 # Translate the observations relative to the midpoint
 p = np.zeros((N_H, 3))
-for i in np.arange(1, N_H + 1).reshape(-1):
+for i in np.arange(0, N_H):
     p[i, :] = P1[i, :] - P_0
 
 # Implement pivot calibration
