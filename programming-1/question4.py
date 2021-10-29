f57769cda328d33654705b1db2cec93e61583451
russell-codingishard <rchow10@jhu.edu>
2021-10-29 02:25:06 -0400

    debug

--- programming-1/question4.py
+++ programming-1/question4.py
@@ -74,14 +74,14 @@ for i in np.arange(0, N_framescal):
 
 # Part C: Compute C_i expected = inv(R_D) * (R_A*ci + p_A - p_D)
 C_exp = np.zeros((N_C, 3, N_framescal))
-for i in np.arange(1, N_framescal + 1).reshape(-1):
-    for j in np.arange(1, N_C + 1).reshape(-1):
-        C_exp[j, :, i] = np.transpose((frameInv(R_D[:, :, i]) * (R_A[:, :, i] * np.transpose(c[j, :]) + p_A[:, i] - p_D[:, i])))
+for i in np.arange(0, N_framescal):
+    for j in np.arange(0, N_C):
+        C_exp[j, :, i] = np.transpose(np.linalg.inv(R_D[:, :, i]) * (R_A[:, :, i] * (c[j]) + p_A[:, :, i] - p_D[:, :, i]))
 
 # Part D: Output C_i expected
 # Reshaping C_exp
 C_final = []
-for i in np.arange(1, N_framescal + 1).reshape(-1):
+for i in np.arange(0, N_framescal):
     C_final = np.array([[C_final], [C_exp[:, :, i]]])
 
 # Export to output.txt file
