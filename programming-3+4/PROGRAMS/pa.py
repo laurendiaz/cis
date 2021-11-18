ecf20515292d1c763360102cfe4ef27d1abb880e
Lauren Diaz <laurendiaz@lauren.local>
2021-11-17 20:54:09 -0500

    implement registration

--- programming-3+4/PROGRAMS/pa.py
+++ programming-3+4/PROGRAMS/pa.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cartesian
+from scipy.spatial.transform import Rotation as R
 
 #implement matching - pa3
 
@@ -12,8 +13,52 @@ def readInput(name):
     return size, data
 
 
-def ICP(body, reading):
-    return 0, 0
+def ICP(A, B):
+    ''' find centroid '''
+    a = np.mean(A, axis=0)
+    b = np.mean(B, axis=0)
+
+    ''' find position of point relative to centroid '''
+    dist_a = np.zeros(A.shape)
+    dist_b = np.zeros(B.shape)
+    for i in np.arange(0, A.shape[0]):
+        dist_a[i,:] = A[i,:] - a
+        dist_b[i,:] = B[i,:] - b
+
+    ''' solve for R '''
+    # compute H
+    H = np.zeros(3)
+    for i in np.arange(0, A.shape[0]):
+        a_curr = dist_a[i,:]
+        b_curr = dist_b[i,:]
+        h_curr = np.array(np.array(
+            a_curr[0] * b_curr[0], a_curr[0] * b_curr[1], a_curr[0] * b_curr[2]
+        ), np.array(
+            a_curr[1] * b_curr[0], a_curr[1] * b_curr[1], a_curr[1] * b_curr[2]
+        ), np.array(
+            a_curr[2] * b_curr[0], a_curr[2] * b_curr[1], a_curr[2] * b_curr[2]
+        ))
+        H = np.concatenate(H, h_curr)
+
+    # compute G
+    delta = np.array(H[1,2] - H[2,1], 
+                H[2,0] - H[0,2],
+                H[0,1] - H[1,0])
+    g = np.array(np.concatenate(np.trace(H), np.transpose(delta)),
+        np.concatenate(delta, H + np.transpose(H) - np.trace(H) * np.eye(3)))
+    
+    # perform eigenvalue decomp
+    v,d = np.linalg.eig(g)
+    dummy, max_i = np.max(np.array(
+        d[0,0], d[1,1], d[2,2], d[3,3]
+    ))
+    quaternion = v[:, max_i[0]]
+    rot = R.from_quat(np.transpose(quaternion))
+
+    ''' solve for p '''
+    pos = np.transpose(b) - R * np.transpose(a)
+
+    return rot, pos
 
 def main():
     print('Enter the name of your input data file (e.g. pa3-a-debug): ')
@@ -38,7 +83,7 @@ def main():
     read_B = np.zeros(size_B, 3, numSamples)
 
     count = 0
-    for i in numSamples:
+    for i in np.arange(0, numSamples):
         read_A[:,:,i] = readingData[count:count + size_A - 1,:]
         count += size_A
         read_B[:,:,i] = readingData[count:count + size_B - 1,:]
@@ -48,7 +93,7 @@ def main():
     ''' compute frame A '''
     rot_A = np.zeros(3,3,numSamples)
     pos_A = np.zeros(3,numSamples)
-    for i in numSamples:
+    for i in np.arange(0, numSamples):
         rot, pos = ICP(body_A, read_A[:,:,i])
         rot_A[:,:,i] = rot
         pos_A[:,i] = pos
@@ -58,7 +103,7 @@ def main():
     ''' compute frame B '''
     rot_B = np.zeros(3,3,numSamples)
     pos_B = np.zeros(3,numSamples)
-    for i in numSamples:
+    for i in np.arange(0, numSamples):
         rot, pos = ICP(body_B, read_B[:,:,i])
         rot_B[:,:,i] = rot
         pos_B[:,i] = pos
